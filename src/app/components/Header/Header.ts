d905ef3625687488fba0c9835a7da6e927755f6b
joaogarin <joao.garin@gmail.com>
2016-01-09 17:05:24 +0100

    Change button colors on header

--- src/app/components/Header/Header.ts
+++ src/app/components/Header/Header.ts
@@ -9,12 +9,12 @@ var header_css = require("!css!sass!./css/_header.scss");
     styles: [`${header_css}`],
     template: `<div class="blog_header">
     <div class="icon-list">
-            <a href="https://www.facebook.com/joao.garin"><i class="fa fa-fw fa-facebook-square"></i></a>
-            <a target="_blank" href="https://github.com/joaogarin"><i class="fa fa-fw fa-github-square"></i></a>
-            <a target="_blank" href="https://www.drupal.org/u/joaogarin"><i class="fa fa-fw fa-drupal"></i></a>
-            <a target="_blank" href="https://twitter.com/joaogarin"><i class="fa fa-fw fa-twitter-square"></i></a>
-            <a target="_blank" href="https://pt.linkedin.com/pub/joao-garin/25/185/9b1"><i class="fa fa-fw fa-linkedin-square"></i></a>
-            <a target="_blank" href="https://www.behance.net/joaogarin"><i class="fa fa-fw fa-behance"></i></a>
+            <a class="hover-bg-facebook" href="https://www.facebook.com/joao.garin"><i class="fa fa-fw fa-facebook-square"></i></a>
+            <a class="hover-bg-github" target="_blank" href="https://github.com/joaogarin"><i class="fa fa-fw fa-github-square"></i></a>
+            <a class="hover-bg-drupal" target="_blank" href="https://www.drupal.org/u/joaogarin"><i class="fa fa-fw fa-drupal"></i></a>
+            <a class="hover-bg-twitter" target="_blank" href="https://twitter.com/joaogarin"><i class="fa fa-fw fa-twitter-square"></i></a>
+            <a class="hover-bg-linkedin" target="_blank" href="https://pt.linkedin.com/pub/joao-garin/25/185/9b1"><i class="fa fa-fw fa-linkedin-square"></i></a>
+            <a class="hover-bg-behance" target="_blank" href="https://www.behance.net/joaogarin"><i class="fa fa-fw fa-behance"></i></a>
         </div>
     </div>`
 })
